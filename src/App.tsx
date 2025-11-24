ff5a1c78d56812b9cd320329d85807c9a67759ff
jhdezpalancar <132522250+jhdezpalancar@users.noreply.github.com>
2025-11-23 23:02:45 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,7 @@ function App() {
       <Offer />
       
       {/* Sticky Mobile CTA */}
-      <div className="fixed bottom-0 left-0 w-full p-4 bg-white/90 backdrop-blur-md border-t border-stone-200 shadow-lg md:hidden z-50">
-      
+      <div className="fixed bottom-0 left-0 w-full p-4 bg-white/90 backdrop-blur-md border-t border-stone-200 shadow-lg md:hidden z-50">  
         <div className="flex justify-center w-full">
           <Button
             href="https://go.hotmart.com/R101103337F?ap=50eb"
@@ -29,8 +28,7 @@ function App() {
           >
             QUIERO EMPEZAR (SOLO $14)
           </Button>
-        </div>
-      
+        </div>      
       </div>
     </main>
   );
